4fab911a04cf9d2d96baf1e96f7bfef99ade5303
Sam Julien <spjulien3@gmail.com>
2022-01-29 11:34:54 -0500

    Base code for api backend

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DomainModels = StudentAdminPortal.API.DomainModels;
+using DataModels = StudentAdminPortal.API.DataModels;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.Student, DomainModels.Student>()
+                .ReverseMap();
+            CreateMap<DataModels.Gender, DomainModels.Gender>()
+                .ReverseMap();
+            CreateMap<DataModels.Address, DomainModels.Address>()
+                .ReverseMap();
+        }
+    }
+}
